e3033c50a4849faefdde5467549f3dba8ec650f9
g-mej <hk591090@ens.uqam.ca>
2020-02-17 04:49:03 -0500

    Scripts: Player control and movement physics added

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    float moveSpeed = 50f;
+
+    float torqueSpeed = -200f;
+
+    float driftPourcentage = 0.95f;
+
     void Start()
     {
-        
     }
 
-    // Update is called once per frame
+    void Update()
+    {
+    }
+
     void FixedUpdate()
     {
-        
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+
+        // Movement
+        if (Input.GetButton("Accelerate"))
+        {
+            rb.AddForce(transform.up * moveSpeed);
+        }
+        else if (Input.GetButton("Break"))
+        {
+            rb.AddForce(transform.up * -0.5f * moveSpeed);
+        }
+
+        rb.velocity = ForwardVelocity() + RightVelocity() * driftPourcentage;
+        rb.angularVelocity = Input.GetAxis("Horizontal") * torqueSpeed;
+    }
+
+    Vector2 ForwardVelocity()
+    {
+        return transform.up *
+        Vector2.Dot(GetComponent<Rigidbody2D>().velocity, transform.up);
+    }
+
+    Vector2 RightVelocity()
+    {
+        return transform.right *
+        Vector2.Dot(GetComponent<Rigidbody2D>().velocity, transform.right);
     }
 }
