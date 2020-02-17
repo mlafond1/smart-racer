39869a50b2132904b2240a63e1611d80f63853db
Mathieu Lafond <mathieu.lafond.18@gmail.com>
2020-02-17 18:36:26 -0500

    Ajout Player Controller

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -4,47 +4,46 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
-    float moveSpeed = 50f;
+    float moveSpeed = 25f;
 
     float torqueSpeed = -200f;
 
     float driftPourcentage = 0.95f;
 
+    float horizontalAxis = 0f;
+
+    Rigidbody2D rb;
+
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    void Update()
-    {
+    public void Accelerate(){
+        rb.AddForce(transform.up * moveSpeed);
+    }
+
+    public void Brake(){
+        rb.AddForce(transform.up * -0.5f * moveSpeed);
+    }
+
+    public void SetAxis(float horizontalAxis){
+        this.horizontalAxis = horizontalAxis;
     }
 
     void FixedUpdate()
     {
-        Rigidbody2D rb = GetComponent<Rigidbody2D>();
-
-        // Movement
-        if (Input.GetButton("Accelerate"))
-        {
-            rb.AddForce(transform.up * moveSpeed);
-        }
-        else if (Input.GetButton("Break"))
-        {
-            rb.AddForce(transform.up * -0.5f * moveSpeed);
-        }
-
         rb.velocity = ForwardVelocity() + RightVelocity() * driftPourcentage;
-        rb.angularVelocity = Input.GetAxis("Horizontal") * torqueSpeed;
+        rb.angularVelocity = horizontalAxis * torqueSpeed;
     }
 
     Vector2 ForwardVelocity()
     {
-        return transform.up *
-        Vector2.Dot(GetComponent<Rigidbody2D>().velocity, transform.up);
+        return transform.up * Vector2.Dot(rb.velocity, transform.up);
     }
 
     Vector2 RightVelocity()
     {
-        return transform.right *
-        Vector2.Dot(GetComponent<Rigidbody2D>().velocity, transform.right);
+        return transform.right * Vector2.Dot(rb.velocity, transform.right);
     }
 }
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    CarController carController;
+
+    void Start(){
+        carController = GetComponent<CarController>();
+    }
+
+    void FixedUpdate(){
+        if(Input.GetButton("Accelerate")) carController.Accelerate();
+        else if(Input.GetButton("Break")) carController.Brake();
+        carController.SetAxis(Input.GetAxis("Horizontal"));
+    }
+}
