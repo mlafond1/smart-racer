0e9fa5d293f0e6cafc263d45b61a0dcfbe9b1283
Eric Gagné <eriga@users.noreply.github.com>
2020-02-28 15:38:20 -0500

    Simple menu de pause + sprites
    -Ajout d'un menu avec fonctions continuer/quitter
    -Ajout de sprites d'autres modèles de véhicules

---
+++ Assets/MenuPause.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPause : MonoBehaviour
+{
+    public static bool partieEnPause = false;
+    public GameObject menuPauseUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (partieEnPause)
+            {
+                Continuer();
+            } else
+            {
+                Pauser();
+            }
+        }    
+    }
+
+    public void Continuer()
+    {
+        menuPauseUI.SetActive(false);
+        Time.timeScale = 1f;
+        partieEnPause = false;
+    }
+
+    void Pauser()
+    {
+        menuPauseUI.SetActive(true);
+        Time.timeScale = 0f;
+        partieEnPause = true;
+    }
+
+    public void Quitter()
+    {
+        Debug.Log("Fermeture du jeu");
+        Application.Quit();
+    }
+
+}
